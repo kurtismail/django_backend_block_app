e525aae6980aa24446a3c8f8d1f7b8db62cf6eb4
ismail kurt <108272463+kurtismail@users.noreply.github.com>
2023-01-04 19:52:58 +0300

    commit

--- main/settings.py
+++ main/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     # myapp
     'blog',
     'user',
     # third party app
+    
     'rest_framework',
     'django_filters',
     'rest_framework.authtoken',
